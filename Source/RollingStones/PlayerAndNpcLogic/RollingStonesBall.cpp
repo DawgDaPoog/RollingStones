41a5972d50d8a5104aac2c24f0b3eeaac21be327
Vlad <Kulini4vlad@ukr.net>
2018-11-30 13:43:54 +0100

    Adding bunch of comments for class clarification.

--- Source/RollingStones/PlayerAndNpcLogic/RollingStonesBall.cpp
+++ Source/RollingStones/PlayerAndNpcLogic/RollingStonesBall.cpp
@@ -51,34 +51,44 @@ ARollingStonesBall::ARollingStonesBall()
 	//// Create a camera and attach to boom
 	//Camera = CreateDefaultSubobject<ACameraActor>(TEXT("Camera0"));
 
-
+	// Create a spark trail that will follow the ball around
 	SparkTrail = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("SparkTrail"));
 	SparkTrail->bAutoActivate = false;
 	SparkTrail->SetupAttachment(RootComponent);
 
+	// Create an enchanced spark trail that is only active when empowered
 	EnchancedSparkTrail = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("EnchancedSparkTrail"));
 	EnchancedSparkTrail->bAutoActivate = false;
 	EnchancedSparkTrail->SetupAttachment(RootComponent);
 	
+	// Idle glow of the ball
 	IdleGlow = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("IdleGlow"));
 	IdleGlow->bAutoActivate = true;
 	IdleGlow->SetupAttachment(RootComponent);
 
+	// Particle effects when charging up
 	ChargeUpEffect = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("ChargeUpEffect"));
 	ChargeUpEffect->bAutoActivate = false;
 	ChargeUpEffect->SetupAttachment(RootComponent);
 	ChargeUpEffect->bAbsoluteRotation = true;
 	
+	// Default subobject for a tiledrop mechanic
 	TileDropMechanic = CreateDefaultSubobject<UTileDropMechanic>(FName("TileDropComponent"));
+
+	// Tag for recognizing and reacting to the player
 	Tags.Add(FName("Player"));
 }
 
 void ARollingStonesBall::BeginPlay()
 {
 	Super::BeginPlay();
+	// Setting chargeup effect size to 0
 	ChargeUpEffect->SetRelativeScale3D(FVector(0));
 
+	// Creating a fluid camera to controll and placing it where it needs to be
 	Camera = GetWorld()->SpawnActor<ACameraActor>(FVector(GetActorLocation() + FVector(-XCameraOffset, 0.f, ZCameraOffset)),FRotator(-80.f,0.f,0.f), FActorSpawnParameters());
+	
+	// Setting basic controller settings during play
 	if (GetController()) {
 		Cast<APlayerController>(GetController())->SetInputMode(FInputModeGameAndUI());
 		Cast<APlayerController>(GetController())->bShowMouseCursor = true;
@@ -92,17 +102,18 @@ void ARollingStonesBall::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	if(bMoving)
-	Ball->AddForce(FVector(UpMovement, RightMovement, 0));
+	// Adding force if we have movement active
+	if (bMoving)
+	{
+		Ball->AddForce(FVector(UpMovement, RightMovement, 0));
+	}
 
+	// Finding overlapping actors and aligning ball to the grid
 	GetOverlappingActors(OverlappingActors);
-
 	AlignToTheGrid();
 
 	// Smoothing the camera movement
-
 	SmoothTheCamera(DeltaTime);
-	
 }
 
 void ARollingStonesBall::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
@@ -122,31 +133,29 @@ void ARollingStonesBall::SetupPlayerInputComponent(class UInputComponent* Player
 	PlayerInputComponent->BindAction("ChooseFirstTileDrop", IE_Pressed, this, &ARollingStonesBall::SetTiledropIndex<0>);
 	PlayerInputComponent->BindAction("ChooseSecondTileDrop", IE_Pressed, this, &ARollingStonesBall::SetTiledropIndex<1>);
 	PlayerInputComponent->BindAction("ChooseThirdTileDrop", IE_Pressed, this, &ARollingStonesBall::SetTiledropIndex<2>);
-
-
 }
 
 void ARollingStonesBall::NotifyHit(UPrimitiveComponent * MyComp, AActor * Other, UPrimitiveComponent * OtherComp, bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult & Hit)
 {
 	Super::NotifyHit(MyComp, Other, OtherComp, bSelfMoved, HitLocation, HitNormal, NormalImpulse, Hit);
 
+	// Upon reacting with the stoptile, play camera shake and stop moving
 	if (MyShake && Other->ActorHasTag("StopTile"))
 	{
 		GetWorld()->GetFirstPlayerController()->PlayerCameraManager->PlayCameraShake(MyShake, 1.0f);
-		UE_LOG(LogTemp, Warning, TEXT("I Hit %s"), *Other->GetName());
+	
+		//Finding the overlapping grid point and snaping our location to it
 		GetOverlappingActors(OverlappingActors);
 		for (auto Actor : OverlappingActors)
 		{
 			if (Actor->ActorHasTag("Pole"))
 			{
-				UE_LOG(LogTemp, Warning, TEXT("Alligning"));
 				auto GridLoc = Actor->GetActorLocation();
 				FVector NewLoc = FVector(GridLoc.X, GridLoc.Y, GetActorLocation().Z);
 				SetActorLocation(NewLoc);
 			}
 		}
 	}
-
 }
 
 void ARollingStonesBall::NotifyActorBeginOverlap(AActor * OtherActor)
@@ -161,25 +170,33 @@ void ARollingStonesBall::DecrementTileDropsLeft()
 
 void ARollingStonesBall::SmoothTheCamera(float DeltaTime)
 {
+	// Smooth camera movement to the point where it needs to be, based on ball's location
 	float XDistance = Camera->GetActorLocation().X - (GetActorLocation().X - XCameraOffset);
 	float YDistance = Camera->GetActorLocation().Y - GetActorLocation().Y;
 	float ZDistance = Camera->GetActorLocation().Z - (GetActorLocation().Z + ZCameraOffset);
 
-	float VelocityAmplifier = 4.f;
+	// How fast the camera is moving towards it's destination
+	const float VelocityAmplifier = 4.f;
 
+	// Getting the velocities
 	float XVelocity = -XDistance;
 	float YVelocity = -YDistance;
 	float ZVelocity = -ZDistance;
 
+	// Setting camera's location slightly closer to it's destination based on the distance and velocity amplification
 	Camera->SetActorLocation(FVector(Camera->GetActorLocation() + FVector(XVelocity, YVelocity, ZVelocity)*DeltaTime*VelocityAmplifier));
 }
 
 // Movement Section Start
 void ARollingStonesBall::StartChargingMovement()
 {
-	if (bIsEmpoweredMovementLocked) { return; }
+	// if the empowered movement is locked, or we have no more empowers left, return
+	if (bIsEmpoweredMovementLocked || AmountOfEmpowersLeft <= 0) { return; }
+
+	// Timer that is used to charge up our movement
 	static FTimerHandle ChargeUpTimer;
-	if (AmountOfEmpowersLeft <= 0) { return; }
+
+	// if the ball is not moving and is not charging, start charging
 	if (!bMoving && !bIsCharging)
 	{
 		bIsCharging = true;
@@ -189,7 +206,7 @@ void ARollingStonesBall::StartChargingMovement()
 		GetWorldTimerManager().SetTimer(ChargeUpTimer, this, &ARollingStonesBall::CompleteChargeUp, 1.f, false, 1.5f);
 		GetWorldTimerManager().SetTimer(ChargeUpParticleTimer, this, &ARollingStonesBall::IncreaseChargeUpParticleEffect, 0.08f, true, 0.f);
 	}
-	else if (!bMoving && bIsCharging)
+	else if (!bMoving && bIsCharging) // Else, if we are not moving and are charging, stop charging
 	{
 		bIsCharging = false;
 
@@ -203,12 +220,16 @@ void ARollingStonesBall::StartChargingMovement()
 
 void ARollingStonesBall::StartMovement(bool IsMovingInX, bool IsNegative)
 {
+	// Stop charging, if we were
 	bIsCharging = false;
 
+	// Clear chargeup timer
 	GetWorldTimerManager().ClearTimer(ChargeUpParticleTimer);
 
+	// Deactivate chargeup particles
 	ChargeUpEffect->Deactivate();
 
+	// if the ball was charged, take of one point from our empowers, activate enchanced spark trail and empowered movement
 	if (bIsCharged)
 	{
 		AmountOfEmpowersLeft--;
@@ -216,23 +237,26 @@ void ARollingStonesBall::StartMovement(bool IsMovingInX, bool IsNegative)
 		bIsEmpowered = true;
 	}
 
+	// Setting values for our charged force, if we had it, and overall force
 	float ChargedForce = bIsCharged * ForceApply / 2;
 	float OverallForce = ChargedForce + ForceApply;
-	if (IsNegative)
-	{
-		RightMovement = -OverallForce * FGenericPlatformMath::Pow(1, (int)IsMovingInX);
-		UpMovement = -OverallForce * (int)IsMovingInX;
-		Ball->AddImpulse(FVector(-OverallForce * (int)IsMovingInX, -OverallForce * FGenericPlatformMath::Pow(1, (int)IsMovingInX), 0.f));
-	}
-	else
-	{
-		RightMovement = OverallForce * FGenericPlatformMath::Pow(1, (int)IsMovingInX);
-		UpMovement = OverallForce * (int)IsMovingInX;
-		Ball->AddImpulse(FVector(OverallForce * (int)IsMovingInX, OverallForce * FGenericPlatformMath::Pow(1, (int)IsMovingInX), 0.f));
-	}
+
+		// determine where this ball was told to move, based on the imputs, and set the movement variables. Add impulse.
+	int32 sign = (IsNegative ? -1 : 1);
+	RightMovement = sign * OverallForce * FGenericPlatformMath::Pow(1, (int)IsMovingInX);
+	UpMovement = sign * OverallForce * (int)IsMovingInX;
+	Ball->AddImpulse(FVector(sign * OverallForce * (int)IsMovingInX, sign * OverallForce * FGenericPlatformMath::Pow(1, (int)IsMovingInX), 0.f));
+
+	// Set charged to false, in case we were
 	bIsCharged = false;
+
+	// We started moving
 	bMoving = true;
+
+	// Increase the particle speed of spark trail
 	SparkTrail->CustomTimeDilation = 1;
+
+	// Unlock the axis we will be moving on
 	if (IsMovingInX)
 	{
 		Ball->SetConstraintMode(EDOFMode::XZPlane);
@@ -243,11 +267,14 @@ void ARollingStonesBall::StartMovement(bool IsMovingInX, bool IsNegative)
 		Ball->SetConstraintMode(EDOFMode::YZPlane);
 		bMovingInYZ = true;
 	}
+
+	// Ignore the invisible walls, while we are moving
 	Ball->SetCollisionProfileName(FName("IgnoreInvisibleWall"));
 }
 
 bool ARollingStonesBall::IsAStopTileBeside(FVector Direction)
 {
+	//
 	if (abs(Direction.X) > 2.f && GetActorLocation().X>0)
 	{
 		Direction.X = -Direction.X;
@@ -264,6 +291,7 @@ bool ARollingStonesBall::IsAStopTileBeside(FVector Direction)
 	//Re-initialize hit info
 	FHitResult RV_Hit(ForceInit);
 
+	// Cast a line trace to determine whether we have a tile beside us or not
 	GetWorld()->LineTraceSingleByChannel(
 		RV_Hit,        //result
 		GetActorLocation(),    //start
@@ -272,19 +300,19 @@ bool ARollingStonesBall::IsAStopTileBeside(FVector Direction)
 		RV_TraceParams
 	);
 
+	// If we hit something
 	if (RV_Hit.bBlockingHit)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Hit Something: %s"), *RV_Hit.GetActor()->GetName());
-
+		// If what we hit was a stop tile
 		if (RV_Hit.GetActor()->ActorHasTag(FName("StopTile")))
 		{
-			UE_LOG(LogTemp, Warning, TEXT("Hit a stop tile"));
+			// If the distance to it is less that 150 ( If we are less than one tile from it)
 			if (FVector::Dist(GetActorLocation(), RV_Hit.GetActor()->GetActorLocation()) < 150) {
-				UE_LOG(LogTemp, Warning, TEXT("Tile is too close. Can't move that way"));
+				// Then the tile is too close, return that we have a tile in that direction
 				return true;
 			}
 			else {
-				UE_LOG(LogTemp, Warning, TEXT("Tile is far away. Can move that way"));
+				// If not, then the tile is far enough and we are able to move in that direction 
 				return false;
 			}
 
@@ -292,14 +320,16 @@ bool ARollingStonesBall::IsAStopTileBeside(FVector Direction)
 	}
 	else
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Hit Nothing"));
+		// What we hit was not a tile, can move
 		return false;
 	}
+	// Hit nothing, can move
 	return false;
 }
 
 void ARollingStonesBall::ResetMovement()
 {
+	// Reseting movement variables, constraints and collisions to idle mode
 	UpMovement = 0;
 	RightMovement = 0;
 	bIsEmpowered = false;
@@ -313,11 +343,12 @@ void ARollingStonesBall::ResetMovement()
 
 void ARollingStonesBall::EnableMovementTimer()
 {
+	// Starting the movement timer
 	static FTimerHandle EnableMovementTimer;
-
 	GetWorldTimerManager().SetTimer(EnableMovementTimer, this, &ARollingStonesBall::EnableMovement, 0.2f, false);
 }
 
+// Functions reacting to the input, asking if we have a tile beside and if not moving, move
 void ARollingStonesBall::MoveRight()
 {
 	if (!bMoving && !IsAStopTileBeside(FVector(1.f, 5.f, 1.f))) {
@@ -374,11 +405,13 @@ void ARollingStonesBall::SetTileDroppingLocked(bool Set)
 
 void ARollingStonesBall::EnableMovement()
 {
+	// Enabling movement possibility
 	bMoving = false;
 }
 
 void ARollingStonesBall::CompleteChargeUp()
 {
+	// Complete chargeup. Clear particle effect increase timer.
 	if (bIsCharging) {
 		bIsCharged = true;
 		GetWorldTimerManager().ClearTimer(ChargeUpParticleTimer);
@@ -387,6 +420,7 @@ void ARollingStonesBall::CompleteChargeUp()
 
 void ARollingStonesBall::AlignToTheGrid()
 {
+	// Adding a force towards tile center, based on the distance to it
 	for (AActor* OverlappingActor : OverlappingActors) {
 		if (OverlappingActor->ActorHasTag(FName("AttractorVolume")) && !bMoving)
 		{
@@ -414,6 +448,7 @@ bool ARollingStonesBall::IsMovingInYZ()
 
 void ARollingStonesBall::Empower()
 {
+	// Making a player empowered, giving him additional movement values and an impulse, if we were not yet empowered
 	if (!bIsEmpowered)
 	{
 		if (bMovingInXZ)
@@ -459,6 +494,7 @@ ACameraActor * ARollingStonesBall::GetWholeViewCamera()
 
 void ARollingStonesBall::RedirectBackwards()
 {
+	// Redirecting the movement of ball backwards
 	RightMovement = -RightMovement;
 	UpMovement = -UpMovement;
 	Ball->AddImpulse(FVector(UpMovement, RightMovement, 0.f));
@@ -466,9 +502,9 @@ void ARollingStonesBall::RedirectBackwards()
 
 void ARollingStonesBall::RedirectSideways(bool bRedirectRight)
 {
+	// Redirecting the movement of ball sideways
 	if (bMovingInXZ)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("MovingInYZ now"));
 		Ball->SetConstraintMode(EDOFMode::YZPlane);
 		bMovingInXZ = false;
 		bMovingInYZ = true;
@@ -490,7 +526,6 @@ void ARollingStonesBall::RedirectSideways(bool bRedirectRight)
 	}
 	else if (bMovingInYZ)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("MovingInXZ now"));
 		Ball->SetConstraintMode(EDOFMode::XZPlane);
 		bMovingInXZ = true;
 		bMovingInYZ = false;
@@ -513,34 +548,43 @@ void ARollingStonesBall::RedirectSideways(bool bRedirectRight)
 
 void ARollingStonesBall::Die()
 {
+	// Sequence for dying when we are hit by something lethal
 	Cast<APlayerController>(GetController())->SetInputMode(FInputModeUIOnly());
+
+	// Deactivating all possible visual clues that we still might be in the world
 	bMoving = false;
 	ResetMovement();
 	SparkTrail->Deactivate();
 	IdleGlow->Deactivate();
 	TileDropMechanic->DestroyComponent();
-
 	Ball->SetVisibility(false);
+
+	// Spawning the game over widget
 	SpawnDeathScreenWidget();
 }
 
  // Assisting Section End
 
+
 // TileDrop Section Start
 void ARollingStonesBall::InitiateTileDrop()
 {
+	// If tile dropping mechanic is locked or we have no tile drops left, return
 	if (bIsTileDroppingLocked || AmountOfTiledropsLeft <=0) { return; }
 	if (TileDropMechanic)
 	{
+		// Initiate tile drop attempt
 		TileDropMechanic->Initiate();
 	}
 }
 
 void ARollingStonesBall::TryTileDrop()
 {
+	// If tile dropping mechanic is locked or we have no tile drops left, return
 	if (bIsTileDroppingLocked || AmountOfTiledropsLeft <= 0) { return; }
 	if (TileDropMechanic)
 	{
+		// Tell a tile to drop selected tile
 		TileDropMechanic->DropSelectedTile();
 	}
 }
@@ -551,6 +595,7 @@ void ARollingStonesBall::TryTileDrop()
 // Remaining
 void ARollingStonesBall::IncreaseChargeUpParticleEffect()
 {
+	// Increasing particle effect size
 	ChargeUpEffect->Activate();
 	ChargeUpEffect->SetRelativeScale3D(ChargeUpEffect->GetRelativeTransform().GetScale3D()+FVector(0.05));
 }
@@ -558,6 +603,7 @@ void ARollingStonesBall::IncreaseChargeUpParticleEffect()
 
 void ARollingStonesBall::SetWholeViewCamera(ACameraActor * CameraToSet)
 {
+	// Setting the camera to blend to when we press whole view button
 	WholeViewCamera = CameraToSet;
 }
 
